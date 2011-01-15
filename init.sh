5514bd2097bb29ddbafb2f65224682faae7be9ae
Andrew Yao <andyao@cloudera.com>
2011-01-14 22:54:40 -0800

    added comments

--- init.sh
+++ init.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-# create symbolic links
+# removed original files
 rm -rf ~/.bashrc
 rm -rf ~/.vimrc
 rm -rf ~/.vim
+# create symbolic links
 ln -s $HOME/github/andrewyao/personal/.bashrc ~/.bashrc
 ln -s $HOME/github/andrewyao/personal/vim ~/.vim
 ln -s $HOME/.vim/.vimrc ~/.vimrc
