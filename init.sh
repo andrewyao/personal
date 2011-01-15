d61a5f04782b4292b23f64b22e97c474388fcb93
Andrew Yao <andyao@cloudera.com>
2011-01-14 22:42:11 -0800

    add init.sh

---
+++ init.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+rm -rf ~/.bashrc
+rm -rf ~/.vimrc
+rm -rf ~/.vim
+ln -s $HOME/github/andrewyao/personal/.bashrc ~/.bashrc
+ln -s $HOME/github/andrewyao/personal/vim ~/.vim
+ln -s $HOME/.vim/.vimrc ~/.vimrc
+
